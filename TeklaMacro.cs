674c9e811c25a7a58e417a9d3d4504597eb9da86
Dale Nicholls <dlnchlls@gmail.com>
2013-09-25 15:01:57 +0100

    First Commit

---
+++ TeklaMacro.cs
@@ -0,0 +1,14 @@
+using Tekla.Structures;
+using Tekla.Structures.Model;
+using Tekla.Structures.Drawing;
+
+namespace Tekla.Technology.Akit.UserScript
+{
+    public class Script
+    {
+        public static void Run(Tekla.Technology.Akit.IScript akit)
+        {
+
+        }
+    }
+}
